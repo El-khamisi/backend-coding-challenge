80dcf3e15719caf8cab702cf575762ce35525ba1
Mohamed El-khamisi <mohammd.alkhamisi@gmail.com>
2021-11-16 21:03:59 +0200

    add mock api response for maintain HTML elements

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,21 @@ const indexController = require('../controllers/indexController');
 
 
 /**
+ * Mock Controller 
+ * 
+ */
+const mockApi = require('../testing/mockAPI.test');
+
+/**
+ * remove comments to try fake routes out
+ */
+router.get('/list', mockApi.getList);
+
+
+/**
  * ----------- GET Routes  -----------
  */
 router.get('/', indexController.getHome);
-router.get('/list', indexController.getList);
+// router.get('/list', indexController.getList);
 
 module.exports = router;
--- view/viewJS.js
+++ view/viewJS.js
@@ -2,6 +2,10 @@ const leaderboard = document.getElementById('leaderboard-body');
 const btn = document.getElementById('leaderboard-header');
 
 
+/**
+ * 
+ * @param {String} URL A string URL you going to use for fetching repositories 
+ */
 async function getData(url = '') {
     console.log(url);
     const response = await fetch(url)
@@ -11,18 +15,18 @@ async function getData(url = '') {
 
             return res.json();
         }).then(data => {
-        leaderboard.innerHTML = '';
-            for(const language in data){
+            leaderboard.innerHTML = '';
+            for (const language in data) {
 
-               leaderboard.insertAdjacentHTML('beforeend',`<main class="leaderboard__profiles">
+                leaderboard.insertAdjacentHTML('beforeend', `
                     <article class="leaderboard__profile">
                         <span class="leaderboard__name">${language}</span>
                         <span class="leaderboard__value">${data[language].count}<span>repositories</span></span>
                         <div class="leaderboard__descripto">askdljfa;sljdf;lsjssssssssssssssssssssssssssssssssssssssssssdf</div>
                      </article>
-                    </main>`);
-                    
-                }
+                   `);
+
+            }
         
 
         }).catch((error) => {
@@ -38,3 +42,4 @@ btn.addEventListener('mouseup', (e) => {
     leaderboard.innerHTML = '<h1>Featching Please Wait .....</h1>';
     getData('/list');
 });
+
